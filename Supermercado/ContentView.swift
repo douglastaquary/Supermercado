7aeb74c8c3c7eee12a1dd9e3fde00ed05861d8df
Douglas Taquary <douglastaquary@gmail.com>
2020-03-29 16:17:54 -0300

    Initial Commit

---
+++ Supermercado/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Supermercado
+//
+//  Created by Douglas Taquary on 29/03/20.
+//  Copyright © 2020 Douglas Taquary. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
